f53e8457cc068e76890cca4b4e089b947d83b89a
OlenaKolyada <olena.kolyada@proton.me>
2026-04-18 03:44:43 +0200

    Project structure with feature modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,3 +5,14 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+}
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+
+include(":feature:analytics")
+include(":feature:catalog")
+include(":feature:workout")
+include(":feature:user")
